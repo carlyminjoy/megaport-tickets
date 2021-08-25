2db2024e2da2f8b13758f37ba045319bf6c7785f
carly <carlylminjoy@gmail.com>
2021-08-25 12:23:54 +1000

    fix host url

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import Header from "./Header.js";
+import Room from "./Room.js";
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+import "./App.css";
+
+const host =
+  process.env.NODE_ENV === "development"
+    ? "localhost"
+    : "megaport-tickets.herokuapp.com";
+
+class App extends Component {
+  ws = new W3CWebSocket(`ws://${host}:5000`);
+
+  render() {
+    console.log("process.env: ", process.env);
+    return (
+      <div className="App">
+        <Header />
+        <Room ws={this.ws} />
+      </div>
+    );
+  }
+}
+
+export default App;

52c3e95968e9fadbf01569438ea299c747dd4b08
carly <carlylminjoy@gmail.com>
2021-08-25 12:52:35 +1000

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ const host =
     : "megaport-tickets.herokuapp.com";
 
 class App extends Component {
-  ws = new W3CWebSocket(`ws://${host}:5000`);
+  ws = new W3CWebSocket(`wss://${host}:5000`);
 
   render() {
     console.log("process.env: ", process.env);
